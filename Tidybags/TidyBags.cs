e31c3a9d5a3ab781add9d305422a524fa1b94b55
LiquidAtoR <LiquidAtoR@HonorBuddy>
2012-12-03 11:15:51 +0100

    v3.6.2.0
    Added Greater Cache of Treasures (Scenario Reward).

--- Tidybags/TidyBags.cs
+++ Tidybags/TidyBags.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Tidy Bags v3.6.1.9 by LiquidAtoR
+ * Tidy Bags v3.6.2.0 by LiquidAtoR
  *
  * This is a trivial little addon that will tidy up on-use items like Clams and
  * Borean Leather Scraps. It uses a stopwatch to stop it spamming Pulse() and
@@ -7,6 +7,9 @@
  *
  * Credits to Ryns, MaiN, erenion, TIA, ShamWOW (Bobby53), Gilderoy and Samrick for their contributions
  * I would also like to thank everyone that has reported items that are added here in the list.
+ *
+ * 2012/12/03  v3.6.2.0
+ *				Added Greater Cache of Treasures (Scenario Reward).
  *
  * 2012/12/02  v3.6.1.9
  *				Added Cache of Mogu Riches (LFR Loot).
@@ -193,7 +196,7 @@ namespace PluginTidyBags3
     {
         public override string Name { get { return "Tidy Bags 3.6 Reloaded"; } }
         public override string Author { get { return "LiquidAtoR"; } }
-        public override Version Version { get { return new Version(3,6,1,9); } }
+        public override Version Version { get { return new Version(3,6,2,0); } }
 		
 		public bool InventoryCheck = false;
 		
@@ -307,12 +310,13 @@ namespace PluginTidyBags3
 			79917, // Worn Monument Ledger (Archaeology)
 			87391, // Plundered Treasure (Luck of the Lotus Buff)
 			88496, // Sealed Crate (MoP version)
-			89613, // Cache of Treasures
+			89613, // Cache of Treasures (Scenario Reward)
 			89804, // Cache of Mogu Riches (LFR Loot)
             89810, // Bounty of a Sundered Land (LFR Contains 25G if you don't win gear)
 			90625, // Treasures of the Vale (Daily Quest Reward)
 			90716, // Good Fortune (when using a Lucky Charm on a boss for loot)
-			90840  // Marauder's Gleaming Sack of Gold (World Boss gold drop)
+			90840, // Marauder's Gleaming Sack of Gold (World Boss gold drop)
+			92813  // Greater Cache of Treasures (Scenario Reward)
         };
 
         private HashSet<uint> _itemUseOnFive = new HashSet<uint>() {
@@ -346,10 +350,11 @@ namespace PluginTidyBags3
 			87391, // Plundered Treasure (Luck of the Lotus Buff)
 			88496, // Sealed Crate (MoP version)
 			89804, // Cache of Mogu Riches (LFR Loot)
-			89613, // Cache of Treasures
+			89613, // Cache of Treasures (Scenario Reward)
 			90625, // Treasures of the Vale (Daily Quest Reward)
 			90716, // Good Fortune
-			90840  // Marauder's Gleaming Sack of Gold (World Boss gold drop)
+			90840, // Marauder's Gleaming Sack of Gold (World Boss gold drop)
+			92813  // Greater Cache of Treasures (Scenario Reward)
         };
 		
 		private HashSet<uint> _destroyItems = new HashSet<uint>() {
